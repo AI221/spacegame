df754f7f406514fa74ad497d72ce2e5960e1be0c
AI221 <aguyshat@gmail.com>
2017-02-25 11:41:07 -0500

    idk im not keeping track of changes

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ struct GridInfo //info for the inaccurate physics pre-calculation
 };
 
 int sGrid[2000][2000]; //TODO: Dynamically sized arrays for both of these
+int dbGrid[2000][2000]; //TODO: Remove this!
 
 int collisionSpots[2000][1]; 
 int nextCollisionSpot = 0;
@@ -41,11 +42,10 @@ class PhysicsObject
 		void setShape(Vector2 shape);
 
 		void tickMyPhysics(); //important: this should NOT have collision detection. that would result on instances where things would collide even though the object they collide with would've moved next tick
-
-
-	private:
 		Vector2 position;
 		Vector2 velocity; 
+
+	private:
 
 		Vector2 newPosition;
 		bool setNewPosition = false;
@@ -64,7 +64,7 @@ class PhysicsObject
 
 
 PhysicsObject* allPhysicsObjects[MAX_PHYSICS_OBJECTS];
-int nextPhysicsObject = 0;
+int nextPhysicsObject = 1; //TODO: Merge with the physicsObjects
 //probably will change this to a dynamically sized array later
 
 
@@ -118,7 +118,7 @@ void PhysicsObject::tickMyPhysics() //the fun part
 			int posy = y+grid.y;
 			if (sGrid[posx][posy] == myNumber) //TODO: Check if necisary
 			{
-				sGrid[posx][posy] = -1;
+				sGrid[posx][posy] = 0;
 			}
 		}
 	}
@@ -139,7 +139,7 @@ void PhysicsObject::tickMyPhysics() //the fun part
 
 	position.x += velocity.x;
 	position.y += velocity.y;
-	//TODO multiple grids, but right now we can't have you go <0
+	//TODO multiple grids, but right now we can't have you go <0 (or 2000< but that'll be fixed too and doesnt happen as much)
 	if (position.x < 0)
 	{
 		position.x = 0;
@@ -157,8 +157,8 @@ void PhysicsObject::tickMyPhysics() //the fun part
 	grid.y = (int) position.y;
 
 	//Calculate how the shape warps. The shape will stretch as you move faster to avoid clipping
-	warpedShape.x = ((velocity.x/10)+1)*grid.shapex;
-	warpedShape.y = ((velocity.y/10)+1)*grid.shapey;
+	warpedShape.x = ((velocity.x/10)+1)*(grid.shapex/10);
+	warpedShape.y = ((velocity.y/10)+1)*(grid.shapey/10);
 
 	for (int x = 0; x < warpedShape.x; x++) 
 	{
@@ -166,7 +166,7 @@ void PhysicsObject::tickMyPhysics() //the fun part
 		{
 			int posx = x+grid.x;
 			int posy = y+grid.y;
-			if (sGrid[posx][posy] == -1) 
+			if (sGrid[posx][posy] == 0) 
 			{
 				sGrid[posx][posy] = myNumber;
 			}
@@ -307,54 +307,115 @@ void RenderedPhysicsObject::render()
 }
 
 
+SDL_Renderer* myRenderer;
+
+
 RenderedPhysicsObject* physicsObjects[100] = {}; //TODO dimensions and what not
-int numPhysicsObjs = 0;
+int numPhysicsObjs = 1; //Starts at 1 because sGrid uses 0 for 'nothing here'
 
 Vector2 camera;
-int camFocusedObj;
+int camFocusedObj = 1;
 
 void render()
 {
 	camera = physicsObjects[camFocusedObj]->myPhysicsObject->getPosition();
 	camera.x -= 640/2;
 	camera.y -= 580/2;
-	for (int i=0; i <= numPhysicsObjs; i++)
+	for (int i=1; i <= numPhysicsObjs; i++)
 	{
 		physicsObjects[i]->render();
 		//TODO: Physics tied to framerate. REEEEEEEEEEEEEEEEEEEEEEEEEEEEEE
 		physicsObjects[i]->tickMyPhysics();
 	}
 }
+
+Sprite* nothingHere;
+Sprite* somethingHere;
+
+void debug_render()
+{ //pulled from my prototype
+	somethingHere->blit({0,0},{10,10});
+	camera = physicsObjects[camFocusedObj]->myPhysicsObject->getPosition();
+//	camera.x -= 640/2;
+//	camera.y -= 580/2;
+	camera.x = camera.x/10;
+	camera.y = camera.y/10;
+	int camerax = 0;//(int) camera.x;
+	int cameray = 0;//(int) camera.y;
+	for (int i = 0; i < 70; i++) { 
+		for (int o = 0; o < 70; o++) {
+			//std::cout << "i " << i <<" t " << i+camerax << std::endl;
+			if ( sGrid[i+camerax][o+cameray] != 0)
+			{
+				somethingHere->blit({i*10,o*10},{0,0});
+				//std::cout << "nothingHere; ";
+			} 
+			else 
+			{
+				nothingHere->blit({i*10,o*10},{0,0});
+				//std::cout << "somethingHere; ";
+			}
+		}
+	}
+
+
+}
+	
  	
 
 
 int main()
 {
-	SDL_Renderer* myRenderer;
+	for (int i = 0; i < 200; i++)
+	{
+		for( int o = 0; o < 200; o++)
+		{
+			sGrid[i][o] = 0;
+			dbGrid[i][o] = 0;
+		}
+	}
+	physicsObjects[0] = NULL;
+
+
+	sGrid[10][10] = 1; //TODO: Remove this!
+
 	SDL_Window* myWindow;
-	Vector2 cam_topointer;
 	SDL_Event event;
 
 
 	camera = {200,0};
+	camFocusedObj = 1;
 
 	
 	myWindow = SDL_CreateWindow("Spacegame",
 	SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 580, 0);
 
 	myRenderer = SDL_CreateRenderer(myWindow, -1, SDL_RENDERER_ACCELERATED);
+
+	//debug bullshit
+ 	nothingHere = new Sprite(myRenderer,"DEBUG_nothingHere.bmp",{10,10});
+	somethingHere = new Sprite(myRenderer,"DEBUG_somethingHere.bmp",{10,10});
 	
 	Sprite* mySprite = new Sprite(myRenderer,"simple.bmp",{25,25});
 
+	Sprite* otherSprite = new Sprite(myRenderer,"DEBUG_somethingHere.bmp",{25,25});
+
+	Sprite* shoddySpaceship = new Sprite(myRenderer,"shottyspaceship.bmp",{25,25});
+
 	Sprite* bg = new Sprite(myRenderer,"DEBUG_nothingHere.bmp",{640,580});
 		
-	physicsObjects[numPhysicsObjs] = new RenderedPhysicsObject(myRenderer,mySprite,&camera,{50,50});	
+	physicsObjects[numPhysicsObjs] = new RenderedPhysicsObject(myRenderer,shoddySpaceship,&camera,{50,50});	
+
 	numPhysicsObjs++;
 	physicsObjects[numPhysicsObjs] = new RenderedPhysicsObject(myRenderer,mySprite,&camera,{200,200});	
 
-	camFocusedObj = 0;
+	numPhysicsObjs++;
+	physicsObjects[numPhysicsObjs] = new RenderedPhysicsObject(myRenderer,otherSprite,&camera,{200,200});	
+	physicsObjects[numPhysicsObjs]->myPhysicsObject->setVelocity({0,1});
+
 	
 	bool keysHeld[323] = {false};
+	bool isDebugRender = false;
 	while (true)
 	{
 		if(SDL_PollEvent(&event))
@@ -370,27 +431,40 @@ int main()
 		}
 		if(keysHeld[SDLK_w])
 		{
-			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({0,-1});
+			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({0,-0.5});
 		}
 		if(keysHeld[SDLK_s])
 		{
-			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({0,1});
+			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({0,0.5});
 		}
 		if(keysHeld[SDLK_d])
 		{
-			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({1,0});
+			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({0.5,0});
 		}
 		if(keysHeld[SDLK_a])
 		{
-			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({-1,0});
+			physicsObjects[camFocusedObj]->myPhysicsObject->addVelocity({-0.5,0});
 		}
 		if(keysHeld[SDLK_1])
 		{
-			camFocusedObj=1;
+			camFocusedObj=2;
 		}
 		if(keysHeld[SDLK_0])
 		{
-			camFocusedObj=0;
+			camFocusedObj=1;
+		}
+		if(keysHeld[SDLK_q])
+		{
+			std::cout <<"x: " << physicsObjects[camFocusedObj]->myPhysicsObject->velocity.x << std::endl;
+			std::cout <<"y: " << physicsObjects[camFocusedObj]->myPhysicsObject->velocity.y << std::endl;
+		}
+		if(keysHeld[SDLK_e])
+		{
+			isDebugRender = true;
+		}
+		if (keysHeld[SDLK_r])
+		{
+			isDebugRender = false;
 		}
 		
 
@@ -403,8 +477,11 @@ int main()
 
 		bg->blit({0,0},{0,0});
 		
-
 		render();
+		if(isDebugRender) //ineffecient but who the fuck cares, it's a goddamn debug render.
+		{
+			debug_render();
+		}
 
 		SDL_RenderPresent(myRenderer);
 		SDL_Delay(16);
